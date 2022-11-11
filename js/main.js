a1d786de6f7b18d8a990942c3dad4af7b8e96ec8
elton <eltonneiferson@outlook.com>
2022-11-11 08:13:21 -0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -10,85 +10,57 @@ const divNine = document.querySelector('.nine')
 const resetGame = document.querySelector('.reset-game')
 
 const options = [
-    '<img class="x" src="imgs/x.png" alt="X" width="140">',
-    '<img class="o" src="imgs/circle.png" alt="O" width="140">',
+    '<img src="imgs/x.png" alt="X" width="140">',
+    '<img src="imgs/circle.png" alt="O" width="140">',
 ]
 
-const localForInsert = [
+const placeToClick = [
     divOne, divTwo, divThree,
     divFour, divFive, divSix,
     divSeven, divEight, divNine
 ]
 
-function randomLocal(){
-    const locals = localForInsert.length
+function randomLocation(){
+    const locals = placeToClick.length
     const randomLocal = Math.floor(Math.random() * locals)
-    let chosenLocation = localForInsert[randomLocal]
-    localForInsert.splice(localForInsert.indexOf(chosenLocation), 1)
-    chosenLocation.innerHTML = options[1]
+    let chosenPlace = placeToClick[randomLocal]
+    placeToClick.splice(placeToClick.indexOf(chosenPlace), 1)
+    chosenPlace.innerHTML = options[1]
+    chosenPlace.classList.add('o')
 }
 
 function click(clickedLocation) {
-    if(localForInsert.includes(clickedLocation) == false){
+    if(placeToClick.includes(clickedLocation) == false){
         return alert('Local já selecionado')
     } else {
         clickedLocation.innerHTML = options[0]
-        localForInsert.splice(localForInsert.indexOf(clickedLocation), 1)
-        if(localForInsert.length == 0){
+        clickedLocation.classList.add('x')
+        placeToClick.splice(placeToClick.indexOf(clickedLocation), 1)
+        if(placeToClick.length == 0){
             return
         }
-        randomLocal()
+        randomLocation()
+    }
+}
+
+function winGame(){
+    if((divOne, divTwo, divThree).classList.contains('x') || (divOne, divTwo, divThree).classList.contains('o')){
+        alert('Parabéns você venceu!')
     }
 }
 
 function reset(){
-    divOne.innerHTML = ''
-    divTwo.innerHTML = ''
-    divThree.innerHTML = ''
-    divFour.innerHTML = ''
-    divFive.innerHTML = ''
-    divSix.innerHTML = ''
-    divSeven.innerHTML = ''
-    divEight.innerHTML = ''
-    divNine.innerHTML = ''
-    localForInsert.length = 0
-    localForInsert.push(divOne, divTwo, divThree, divFour, divFive, divSix, divSeven, divEight, divNine)
+    placeToClick.forEach((item)=> {
+        item.innerHTML = ''
+    })
+    placeToClick.length = 0
+    placeToClick.push(divOne, divTwo, divThree, divFour, divFive, divSix, divSeven, divEight, divNine)
 }
 
-divOne.addEventListener('click', () => {
-    click(divOne)
-})
-
-divTwo.addEventListener('click', () => {
-    click(divTwo)
-})
-
-divThree.addEventListener('click', () => {
-    click(divThree)
-})
-
-divFour.addEventListener('click', () => {
-    click(divFour)
-})
-
-divFive.addEventListener('click', () => {
-    click(divFive)
-})
-
-divSix.addEventListener('click', () => {
-    click(divSix)
-})
-
-divSeven.addEventListener('click', () => {
-    click(divSeven)
-})
-
-divEight.addEventListener('click', () => {
-    click(divEight)
-})
-
-divNine.addEventListener('click', () => {
-    click(divNine)
+placeToClick.forEach((placeClicked) => {
+    placeClicked.addEventListener('click', () => {
+        click(placeClicked)
+    })
 })
 
 resetGame.addEventListener('click', reset)
